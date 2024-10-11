a742f5a271983f4989ac92ba1d0eb3d439cc350f
Gaurav Kumar <130595382+GauravKumar1w@users.noreply.github.com>
2024-10-12 03:49:38 +0530

    customer.js

--- server/model/customer.js
+++ server/model/customer.js
@@ -1,16 +1,19 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose"); // Import the mongoose library for MongoDB object modeling
 
-var userSchema= new mongoose.Schema({
- RIG:{
-        type: String,
-        required: [true, "User ID is required"]
+// Define a schema for the employee (user) model
+var userSchema = new mongoose.Schema({
+    RIG: {
+        type: String, // Define the RIG field as a string
+        required: [true, "User ID is required"] // User ID is required
     },
-password: {
-        type: String,
-        required: [true, "Password is required"]
+    password: {
+        type: String, // Define the password field as a string
+        required: [true, "Password is required"] // Password is required
     }
-})
+});
 
-const CustomerDB= mongoose.model('customerDB', userSchema);
+// Create a model named 'customerDB' based on the userSchema
+const CustomerDB = mongoose.model('customerDB', userSchema);
 
+// Export the CustomerDB model for use in other parts of the application
 module.exports = CustomerDB;
