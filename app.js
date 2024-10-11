f6b0783075e7801822e3395cf41a7314b3f758bd
Gaurav Kumar <130595382+GauravKumar1w@users.noreply.github.com>
2024-10-12 03:47:22 +0530

    app.js

--- app.js
+++ app.js
@@ -1,134 +1,73 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const request = require("request");
-const dotenv = require("dotenv");
 const app = express();
-const ejs = require("ejs");
 const md5 = require("md5");
-const path = require("path");
 const mongoose = require("mongoose");
 
+// Connect to MongoDB
+mongoose.connect('mongodb://127.0.0.1:27017/transport', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to database")) // Success message
+  .catch(err => console.log(err)); // Error logging
 
-mongoose.connect('mongodb://127.0.0.1:27017/transport', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-  console.log("Connected to database");
-}).catch((err) => {
-  console.log(err);
-});
-app.set("view engine", "ejs");
-app.set("views", "public");
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static("public"));
+app.set("view engine", "ejs"); // Set view engine
+app.set("views", "public"); // Views directory
+app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded bodies
+app.use(express.json()); // Parse JSON bodies
+app.use(express.static("public")); // Serve static files
 
-// Load routers
-app.use("/", require("./server/routes/router"));
+// Route handlers
+app.use("/", require("./server/routes/router")); // Load routes
 
-// Authentication
-const adminDB = require("./server/model/admin");
-const customerDB = require("./server/model/customer");
-const { redirect } = require("express/lib/response");
+// MongoDB models
+const adminDB = require("./server/model/admin"); // Admin model
+const customerDB = require("./server/model/customer"); // Customer model
 
-// Admin signup hidden
-app.get("/register", function (req, res) {
-  res.render("register");
-});
+// Admin registration
+app.get("/register", (req, res) => res.render("register")); // Render registration page
 
+// Admin login
+app.get("/login", (req, res) => res.render("login")); // Render login page
 
-
-// Admin Login
-app.get("/login", function (req, res) {
-  res.render("login");
-});
-
-//Admin login
-app.post("/login", function (req, res) {
-  const username = req.body.RIG;
-  const password = md5(req.body.password);
-  adminDB.findOne({ RIG: username }, function (err, foundUser) {
-    if (err) res.redirect("/failure");
-    else {
-      if (foundUser) {
-        if (foundUser.password === password) res.redirect("/managerview");
-        else res.redirect("/failure");
-      }
-    }
+// Process admin login
+app.post("/login", (req, res) => {
+  const username = req.body.RIG; // Get username
+  const password = md5(req.body.password); // Hash password
+  adminDB.findOne({ RIG: username }, (err, foundUser) => {
+    if (err) res.redirect("/failure"); // Error handling
+    else if (foundUser && foundUser.password === password) res.redirect("/managerview"); // Match
+    else res.redirect("/failure"); // No match
   });
 });
 
-//User signup
-app.get("/createnewuser", function (req, res) {
-  res.render("createnewuser");
-});
+// Employee registration
+app.get("/createnewuser", (req, res) => res.render("createnewuser")); // Render user creation page
 
-//Employee signup
-app.post("/createnewuser", function (req, res) {
-  const newAdmin = new customerDB({
+// Process employee registration
+app.post("/createnewuser", (req, res) => {
+  const newEmp = new customerDB({ // Create new employee
     RIG: req.body.RIG,
-    password: md5(req.body.password),
+    password: md5(req.body.password), // Hash password
   });
-
-  newAdmin.save(function (err) {
-    if (err) res.redirect("/failure");
-    else {
-      //alert("successfully registered");
-      res.redirect("/login");
-    }
-  });
+  newEmp.save(err => err ? res.redirect("/failure") : res.redirect("/login")); // Save and redirect
 });
 
-// User Login
-app.get("/managerlogin", function (req, res) {
-  res.render("managerlogin");
-});
+// Employee login
+app.get("/managerlogin", (req, res) => res.render("managerlogin")); // Render manager login page
 
-//User Login
-app.post("/managerlogin", function (req, res) {
-  const username = req.body.RIG;
-  const password = md5(req.body.password);
-  customerDB.findOne({ RIG: username }, function (err, foundUser) {
-    if (err) res.redirect("/failure");
-    else {
-      if (foundUser) {
-        if (foundUser.password === password) res.redirect("/view");
-        else res.redirect("/failure");
-      }
-    }
+// Process employee login
+app.post("/managerlogin", (req, res) => {
+  const username = req.body.RIG; // Get username
+  const password = md5(req.body.password); // Hash password
+  customerDB.findOne({ RIG: username }, (err, foundUser) => {
+    if (err) res.redirect("/failure"); // Error handling
+    else if (foundUser && foundUser.password === password) res.redirect("/view"); // Match
+    else res.redirect("/failure"); // No match
   });
 });
-// for payment purpose
-// app.post("/checktoken",function(req,res){
-// 	//const token=req.body.paytoken;
 
-// 	    //  if(token==="token123")
-// 		{
-// 			// if(token==="token123")
-// 		{res.redirect("/seetruckdetail?id="+ req.body.id);
-// 				//   {res.redirect("/seecasedetail/"+req.body.id);
-// 		}
-// 				// else
-// 				// 	res.redirect("/failure");
 
-// 		}
+// Payment processing route
+app.post("/makepayment", (req, res) => res.redirect("/makepayment")); // Redirect to payment
 
-// })
-app.post("/managerloginfun", function (req, res) {
-  const username = req.body.RIG;
-  const password = md5(req.body.password);
-  adminDB.findOne({ RIG: username }, function (err, foundUser) {
-    if (err) res.redirect("/failure");
-    else {
-      if (foundUser) {
-        if (foundUser.password === password) res.redirect("/managerview");
-        else res.redirect("/failure");
-      }
-    }
-  });
-});
-// // for payment purpose
-app.post("/makepayment", function (req, res) {
-  res.redirect("/makepayment");
-});
-
-app.listen(5000, function (req, res) {
-  console.log("server running on port 5000");
-});
+// Start server
+app.listen(5000, () => console.log("server running on port 5000")); // Log server status
