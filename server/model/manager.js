e34b84281eb44388559875830bd8bbf3000e92dc
Gaurav Kumar <130595382+GauravKumar1w@users.noreply.github.com>
2024-10-12 03:50:04 +0530

    manager.js

--- server/model/manager.js
+++ server/model/manager.js
@@ -1,23 +1,26 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
-var schema= new mongoose.Schema({
- mno:{
- 	type: String
- },
- mname:{
- 	type: String
- },
- leaveFrom:{
- 	type: String
- },
- leaveTill:{
- 	type: String
- },
- nature:{
- 	type: String
- }
-})
+// Define the schema for manager (employee) details
+var schema = new mongoose.Schema({
+    mno: { // Employee number
+        type: String // Data type is String
+    },
+    mname: { // Employee name
+        type: String // Data type is String
+    },
+    leaveFrom: { // Start date of leave
+        type: String // Data type is String
+    },
+    leaveTill: { // End date of leave
+        type: String // Data type is String
+    },
+    nature: { // Nature of leave
+        type: String // Data type is String
+    }
+});
 
+// Create a Mongoose model using the defined schema
 const Managerdb = mongoose.model('managerdb', schema);
 
+// Export the model for use in other parts of the application
 module.exports = Managerdb;
