ac061d865d049bf38f77ad62a2366f51b34d0880
Gaurav Kumar <130595382+GauravKumar1w@users.noreply.github.com>
2024-10-12 03:51:38 +0530

    actions.js

--- public/scripts/actions.js
+++ public/scripts/actions.js
@@ -1,48 +1,51 @@
-$("#add_user").submit(function(event){
-    alert("Data Inserted Successfully!");
-})
+// Handle the submission of the form to add a new consignment
+$("#add_user").submit(function(event) {
+    alert("Data Inserted Successfully!");  // Alert on successful data insertion
+});
 
+// Handle the submission of the form to update an existing consignment
+$("#update_user").submit(function(event) {
+    event.preventDefault();  // Prevent default form submission behavior
 
+    var unindexed_array = $(this).serializeArray();  // Serialize form data into an array
+    var data = {};
 
-$("#update_user").submit(function(event){
-    event.preventDefault();
+    // Map the serialized array into a key-value object
+    $.map(unindexed_array, function(n, i) {
+        data[n['name']] = n['value'];  // Create an object with form data
+    });
 
-    var unindexed_array = $(this).serializeArray();
-    var data = {}
-
-    $.map(unindexed_array, function(n, i){
-        data[n['name']] = n['value']
-    })
-
-
+    // Prepare the AJAX request for updating the consignment
     var request = {
-        "url" : `http://localhost:5000/api/users/${data.id}`,
-        "method" : "PUT",
-        "data" : data
-    }
+        "url": `http://localhost:5000/api/users/${data.id}`,  // URL with the consignment ID
+        "method": "PUT",  // HTTP method for update
+        "data": data  // Data to be sent in the request
+    };
 
-    $.ajax(request).done(function(response){
-        alert("Data Updated Successfully!");
-    })
+    // Execute the AJAX request
+    $.ajax(request).done(function(response) {
+        alert("Data Updated Successfully!");  // Alert on successful update
+    });
+});
 
-})
+// If the current path is for viewing consignments
+if (window.location.pathname == "/view") {
+    $ondelete = $(".table .myTable td a.delete");  // Select delete links in the table
+    $ondelete.click(function() {
+        var id = $(this).attr("data-id");  // Get the ID of the consignment to delete
 
-if(window.location.pathname == "/view"){
-    $ondelete = $(".table .myTable td a.delete");
-    $ondelete.click(function(){
-        var id = $(this).attr("data-id")
-
+        // Prepare the AJAX request for deletion
         var request = {
-            "url" : `http://localhost:5000/api/users/${id}`,
-            "method" : "DELETE"
-        }
+            "url": `http://localhost:5000/api/users/${id}`,  // URL with the consignment ID
+            "method": "DELETE"  // HTTP method for delete
+        };
 
-        if(confirm("Do you really want to delete this record?")){
-            $.ajax(request).done(function(response){
-                alert("Data Deleted Successfully!");
-                location.reload();
-            })
+        // Confirm deletion action
+        if (confirm("Do you really want to delete this record?")) {
+            $.ajax(request).done(function(response) {
+                alert("Data Deleted Successfully!");  // Alert on successful deletion
+                location.reload();  // Reload the page to reflect changes
+            });
         }
-
-    })
+    });
 }
